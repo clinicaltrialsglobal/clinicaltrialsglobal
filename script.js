c51e9f9083cd685a63e7ead930f85d018d76297b
clinicaltrialsglobal <139549710+clinicaltrialsglobal@users.noreply.github.com>
2023-07-31 20:28:08 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript functions to handle button clicks
+function goToHomePage() {
+    window.location.href = "index.html";
+}
+
+function goToWorldTrials() {
+    window.location.href = "world_trials.html";
+}
+
+function goToUKTrials() {
+    window.location.href = "uk_trials.html";
+}
+
+function goToCancerTrials() {
+    window.location.href = "cancer_trials.html";
+}
